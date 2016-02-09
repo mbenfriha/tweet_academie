3c72035b62f6c715f47792a92b52592d2c130206
meille_r <robin.meillet@epitech.eu>
2016-02-09 02:11:39 +0100

    Fork

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# tweet_academie
+# tweet_academie database
+
+Base de donnée WAC/Epitech - Projet Tweet Academie
+
+# Dernières modifications (Robin, 09/02/2015) :
+
+- Les INT passe tous en UNSIGNED
+- Suppression des INDEX (clef grise) sur les ID primaire
+- Renommage des champs "date" en champ plus nominatif (fav_date, messsage_date, ...)
+- Ajout de champ date manquant sur certaines tables
+- "picture" deviens "picture_url"
+- Le champ "content" dans "tp_messages" passe en TEXT (twitter autorise depuis bien longtemps les messages privés de plus de 140 caractères)
+- Passage en UTF-8 de tout les champs plutôt que LATIN
+- De nombreux champs de la table "tp_users" deviennent optionnel (NULL autorisé) pour permettre l'inscription en 2 temps si nécessaire
+- Ajouts des relations en cascade sur toutes les clef étrangères
--- common-database.sql
+++ common-database.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.5.4.1
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost:3306
--- Généré le :  Jeu 04 Février 2016 à 11:06
--- Version du serveur :  5.5.42
--- Version de PHP :  5.6.10
+-- Client :  localhost
+-- Généré le :  Mar 09 Février 2016 à 01:59
+-- Version du serveur :  5.5.47-0ubuntu0.14.04.1
+-- Version de PHP :  5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de données :  `tweets`
+-- Base de données :  `tweeter`
 --
 
 -- --------------------------------------------------------
@@ -21,10 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tp_favoris` (
-  `id` int(10) unsigned NOT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  `tweet_id` int(11) unsigned NOT NULL,
-  `date_like` datetime NOT NULL
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `tweet_id` int(11) UNSIGNED NOT NULL,
+  `fav_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -34,10 +40,10 @@ CREATE TABLE `tp_favoris` (
 --
 
 CREATE TABLE `tp_follow` (
-  `id` int(10) unsigned NOT NULL,
-  `follow_id` int(11) unsigned NOT NULL,
-  `follower_id` int(11) unsigned NOT NULL,
-  `date` datetime NOT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `follow_id` int(11) UNSIGNED NOT NULL,
+  `follower_id` int(11) UNSIGNED NOT NULL,
+  `follow_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -47,14 +53,15 @@ CREATE TABLE `tp_follow` (
 --
 
 CREATE TABLE `tp_messages` (
-  `id` int(11) unsigned NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `destinataire_id` int(11) unsigned NOT NULL,
-  `expediteur_id` int(11) unsigned NOT NULL,
-  `destinataire_del` tinyint(1) unsigned NOT NULL,
-  `expediteur_del` tinyint(1) unsigned NOT NULL,
-  `view` tinyint(1) unsigned NOT NULL,
-  `date` datetime NOT NULL
+  `id` int(11) UNSIGNED NOT NULL,
+  `content` text NOT NULL,
+  `destinataire_id` int(11) UNSIGNED NOT NULL,
+  `expediteur_id` int(11) UNSIGNED NOT NULL,
+  `destinataire_del` tinyint(1) UNSIGNED NOT NULL,
+  `expediteur_del` tinyint(1) UNSIGNED NOT NULL,
+  `view` tinyint(1) UNSIGNED NOT NULL,
+  `view_date` datetime NOT NULL,
+  `message_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,10 +71,11 @@ CREATE TABLE `tp_messages` (
 --
 
 CREATE TABLE `tp_pictures` (
-  `id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `tweet_id` int(10) unsigned NOT NULL,
-  `picture` varchar(255) NOT NULL
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `tweet_id` int(11) UNSIGNED NOT NULL,
+  `picture_url` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `picture_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -77,7 +85,7 @@ CREATE TABLE `tp_pictures` (
 --
 
 CREATE TABLE `tp_preference` (
-  `id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
   `theme` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -88,11 +96,11 @@ CREATE TABLE `tp_preference` (
 --
 
 CREATE TABLE `tp_replys` (
-  `id` int(11) unsigned NOT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  `tweet_id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `tweet_id` int(11) UNSIGNED NOT NULL,
   `content` text NOT NULL,
-  `date` datetime NOT NULL
+  `reply_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -102,10 +110,10 @@ CREATE TABLE `tp_replys` (
 --
 
 CREATE TABLE `tp_tweets` (
-  `id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
   `content` varchar(140) NOT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  `created` date NOT NULL
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `tweet_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -115,24 +123,23 @@ CREATE TABLE `tp_tweets` (
 --
 
 CREATE TABLE `tp_users` (
-  `id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
   `login` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `theme` int(11) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `country` varchar(255) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
   `connect` tinyint(1) NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `birthday` date NOT NULL,
+  `birthday` date DEFAULT NULL,
   `register` datetime NOT NULL,
   `last_connection` datetime NOT NULL,
-  `sexe` enum('m','f') NOT NULL,
-  `cover` varchar(255) NOT NULL,
-  `avatar` varchar(255) NOT NULL
+  `sexe` enum('m','f') DEFAULT NULL,
+  `cover` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -142,9 +149,9 @@ CREATE TABLE `tp_users` (
 --
 
 CREATE TABLE `tp_user_preference` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `preference_id` int(11) NOT NULL
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `preference_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -156,44 +163,46 @@ CREATE TABLE `tp_user_preference` (
 --
 ALTER TABLE `tp_favoris`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`user_id`);
+  ADD KEY `id_user` (`user_id`),
+  ADD KEY `tweet_id` (`tweet_id`);
 
 --
 -- Index pour la table `tp_follow`
 --
 ALTER TABLE `tp_follow`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `follow_id` (`follow_id`),
+  ADD KEY `follower_id` (`follower_id`);
 
 --
 -- Index pour la table `tp_messages`
 --
 ALTER TABLE `tp_messages`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `destinataire_id` (`destinataire_id`),
+  ADD KEY `expediteur_id` (`expediteur_id`);
 
 --
 -- Index pour la table `tp_pictures`
 --
 ALTER TABLE `tp_pictures`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tweet_id` (`tweet_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Index pour la table `tp_preference`
 --
 ALTER TABLE `tp_preference`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_preference` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `tp_replys`
 --
 ALTER TABLE `tp_replys`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_reply` (`id`),
-  ADD KEY `id__user` (`user_id`),
-  ADD KEY `id_tweet` (`tweet_id`),
-  ADD KEY `id_reply_2` (`id`),
-  ADD KEY `id__user_2` (`user_id`),
-  ADD KEY `id_tweet_2` (`tweet_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `tweet_id` (`tweet_id`);
 
 --
 -- Index pour la table `tp_tweets`
@@ -208,8 +217,7 @@ ALTER TABLE `tp_tweets`
 -- Index pour la table `tp_users`
 --
 ALTER TABLE `tp_users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `tp_user_preference`
@@ -228,44 +236,98 @@ ALTER TABLE `tp_user_preference`
 -- AUTO_INCREMENT pour la table `tp_favoris`
 --
 ALTER TABLE `tp_favoris`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_follow`
 --
 ALTER TABLE `tp_follow`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_messages`
 --
 ALTER TABLE `tp_messages`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_pictures`
 --
 ALTER TABLE `tp_pictures`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_preference`
 --
 ALTER TABLE `tp_preference`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_replys`
 --
 ALTER TABLE `tp_replys`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_tweets`
 --
 ALTER TABLE `tp_tweets`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `tp_users`
 --
 ALTER TABLE `tp_users`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `tp_user_preference`
 --
 ALTER TABLE `tp_user_preference`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `tp_favoris`
+--
+ALTER TABLE `tp_favoris`
+  ADD CONSTRAINT `tp_favoris_ibfk_2` FOREIGN KEY (`tweet_id`) REFERENCES `tp_tweets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tp_favoris_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `tp_follow`
+--
+ALTER TABLE `tp_follow`
+  ADD CONSTRAINT `tp_follow_ibfk_2` FOREIGN KEY (`follower_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tp_follow_ibfk_1` FOREIGN KEY (`follow_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `tp_messages`
+--
+ALTER TABLE `tp_messages`
+  ADD CONSTRAINT `tp_messages_ibfk_2` FOREIGN KEY (`expediteur_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tp_messages_ibfk_1` FOREIGN KEY (`destinataire_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `tp_pictures`
+--
+ALTER TABLE `tp_pictures`
+  ADD CONSTRAINT `tp_pictures_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tp_pictures_ibfk_1` FOREIGN KEY (`tweet_id`) REFERENCES `tp_tweets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `tp_replys`
+--
+ALTER TABLE `tp_replys`
+  ADD CONSTRAINT `tp_replys_ibfk_1` FOREIGN KEY (`tweet_id`) REFERENCES `tp_tweets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `tp_tweets`
+--
+ALTER TABLE `tp_tweets`
+  ADD CONSTRAINT `tp_tweets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `tp_user_preference`
+--
+ALTER TABLE `tp_user_preference`
+  ADD CONSTRAINT `tp_user_preference_ibfk_2` FOREIGN KEY (`preference_id`) REFERENCES `tp_preference` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tp_user_preference_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
