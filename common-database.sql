77b853861a818ffc8cfb5560ef8392c8cb9e57fb
benfri_m <mohamed.benfriha@epitech.eu>
2016-02-11 14:14:28 +0100

    add retweet table

--- common-database.sql
+++ common-database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Mar 09 Février 2016 à 09:21
+-- Généré le :  Jeu 11 Février 2016 à 14:13
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -56,7 +56,18 @@ CREATE TABLE `tp_messages` (
   `view` tinyint(1) unsigned NOT NULL,
   `view_date` datetime NOT NULL,
   `message_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `tp_messages`
+--
+
+INSERT INTO `tp_messages` (`id`, `content`, `destinataire_id`, `expediteur_id`, `destinataire_del`, `expediteur_del`, `view`, `view_date`, `message_date`) VALUES
+(1, 'coucou', 7, 8, 0, 0, 0, '0000-00-00 00:00:00', '2016-02-09 00:00:00'),
+(2, 'salut sa va ?', 8, 7, 0, 0, 0, '0000-00-00 00:00:00', '2016-02-11 00:00:00'),
+(3, 'wesh repond', 7, 5, 0, 0, 0, '0000-00-00 00:00:00', '2016-02-18 00:00:00'),
+(4, 'we jsuis la', 5, 7, 0, 0, 0, '2016-02-25 00:00:00', '2016-02-24 00:00:00'),
+(5, 'tu fou quoi ?', 7, 5, 0, 0, 0, '2016-02-16 00:00:00', '2016-02-25 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -100,6 +111,20 @@ CREATE TABLE `tp_replys` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `tp_retweets`
+--
+
+CREATE TABLE `tp_retweets` (
+  `id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `tweet_id` int(11) unsigned NOT NULL,
+  `content` varchar(140) CHARACTER SET utf8 NOT NULL,
+  `date_retweet` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `tp_tweets`
 --
 
@@ -138,7 +163,16 @@ CREATE TABLE `tp_users` (
   `sexe` enum('m','f') DEFAULT NULL,
   `cover` varchar(255) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `tp_users`
+--
+
+INSERT INTO `tp_users` (`id`, `login`, `email`, `password`, `token`, `first_name`, `last_name`, `cpostal`, `adress`, `departement`, `region`, `city`, `country`, `connect`, `active`, `birthday`, `register_date`, `last_connection`, `sexe`, `cover`, `avatar`) VALUES
+(5, 'test', 'test@t.fr', 'fb2e8d11eebd1df4f8627fbe7246bc2759c1d595', 'e96f7e3a7d7fea9a8f1d200fc3451af2', 'est', 'test', NULL, NULL, NULL, NULL, 'test', 'test', 0, 0, NULL, '2016-02-09 14:55:48', '2016-02-09 14:55:48', NULL, NULL, NULL),
+(7, 'root', 'root@root.fr', 'f561b1a3476aa8e5d2902044722e08bd5886c649', '40ce4314247c6b06d300b26cd79ae671', 'patchouni', 'test', NULL, NULL, NULL, NULL, 'test', 'test', 0, 0, NULL, '2016-02-09 16:34:08', '2016-02-09 16:34:08', NULL, NULL, NULL),
+(8, 'patchouni', 'aka68@hotmail.fr', 'f561b1a3476aa8e5d2902044722e08bd5886c649', '04cafd561412f737e9f1bfcb3e6e3a14', 'ferz', 'zerfze', NULL, NULL, NULL, NULL, 'zrfez', 'fzerf', 0, 0, NULL, '2016-02-10 15:06:26', '2016-02-10 15:06:26', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -203,6 +237,14 @@ ALTER TABLE `tp_replys`
   ADD KEY `tweet_id` (`tweet_id`);
 
 --
+-- Index pour la table `tp_retweets`
+--
+ALTER TABLE `tp_retweets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `tweet_id` (`tweet_id`);
+
+--
 -- Index pour la table `tp_tweets`
 --
 ALTER TABLE `tp_tweets`
@@ -246,7 +288,7 @@ ALTER TABLE `tp_follow`
 -- AUTO_INCREMENT pour la table `tp_messages`
 --
 ALTER TABLE `tp_messages`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `tp_pictures`
 --
@@ -263,6 +305,11 @@ ALTER TABLE `tp_preference`
 ALTER TABLE `tp_replys`
   MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `tp_retweets`
+--
+ALTER TABLE `tp_retweets`
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `tp_tweets`
 --
 ALTER TABLE `tp_tweets`
@@ -271,7 +318,7 @@ ALTER TABLE `tp_tweets`
 -- AUTO_INCREMENT pour la table `tp_users`
 --
 ALTER TABLE `tp_users`
-  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `tp_user_preference`
 --
@@ -316,10 +363,11 @@ ALTER TABLE `tp_replys`
   ADD CONSTRAINT `tp_replys_ibfk_1` FOREIGN KEY (`tweet_id`) REFERENCES `tp_tweets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `tp_tweets`
+-- Contraintes pour la table `tp_retweets`
 --
-ALTER TABLE `tp_tweets`
-  ADD CONSTRAINT `tp_tweets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tp_retweets`
+  ADD CONSTRAINT `tp_retweets_ibfk_2` FOREIGN KEY (`tweet_id`) REFERENCES `tp_tweets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tp_retweets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `tp_user_preference`
